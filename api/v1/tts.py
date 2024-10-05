7530155d1c680f90a80b7a089f680a614b2b0237
JoshiParSa <joshiparsa@gmail.com>
2024-10-05 01:14:03 -0500

    tts beta complete

--- api/v1/tts.py
+++ api/v1/tts.py
@@ -1,8 +1,11 @@
 from fastapi import APIRouter
+from fastapi.responses import Response
 from core.tts_processor import process_data
 
 router = APIRouter()
 
+
 @router.post("/upload")
 async def upload(data: dict):
-    return await process_data(data)
+    audio_buffer = await process_data(data)
+    return Response(content=audio_buffer.getvalue(), media_type="audio/wav")
--- core/tts_processor.py
+++ core/tts_processor.py
@@ -1,7 +1,33 @@
-#import pyttsx
+import pyttsx3
+import hashlib
+import json
+import io
 
-#text_input = open(r"res/text_input.txt")
+
+def hash_json(data):
+    json_string = json.dumps(data, sort_keys=True).encode("utf-8")
+    hash_object = hashlib.sha256(json_string)
+    hash_hex = hash_object.hexdigest()
+    return hash_hex
+
+
+def json_to_text(data) -> str:
+    text_output = data["tag"]
+    for student in data["data"]:
+        text_output += f"\nEstudiante con codigo {student['codigo']}, {student['name']} {student['f-surname']} {student['m-surname']} tiene una nota de {student['grade']}."
+    return text_output
+
 
 async def process_data(data):
-    print(data)
-    
+    engine = pyttsx3.init()
+    engine.setProperty("rate", 130)
+    audio_buffer = io.BytesIO()
+
+    engine.save_to_file(json_to_text(data), "students_audio.wav")
+    engine.runAndWait()
+
+    with open("students_audio.wav", "rb") as f:
+        audio_buffer.write(f.read())
+    audio_buffer.seek(0)
+
+    return audio_buffer
